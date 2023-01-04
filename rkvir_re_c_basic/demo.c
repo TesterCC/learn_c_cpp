f136f104ca981b2154841e9c8b63f1725debef2b
TesterCC <testerlyx@foxmail.com>
2023-01-04 23:01:57 +0800

    update - c demo

---
+++ rkvir_re_c_basic/demo.c
@@ -0,0 +1,11 @@
+// https://www.bilibili.com/video/BV1er4y1u7Ya/
+// A Simple Demo
+#include <stdio.h>   // 标准输入输出头文件
+int main(void)       // 主函数main
+{
+    int num;
+    num = 1;
+    printf("I am simple!");
+    return 0;
+}
+
